fecb858067e1afbf06d270ba85f074c52195ed30
JayB111 <jay.ljungby@gmail.com>
2025-06-22 12:32:49 +0200

    Removed WeatherForecast template files and added QuestionsController

---
+++ backend/backend/Controllers/QuestionsController.cs
@@ -0,0 +1,33 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public QuestionsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+        {
+            return await _context.Questions.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Question>> PostQuestion(Question question)
+        {
+            _context.Questions.Add(question);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetQuestions), new { id = question.Id }, question);
+        }
+    }
+}
+
